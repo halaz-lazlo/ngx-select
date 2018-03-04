2042748392e0fb743760f204b13d30645c901f1f
halaz.lazlo@gmail.com <halaz.lazlo@gmail.com>
2018-03-04 11:52:36 +0100

    index.ts added

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { NgxSelectModule } from "./src/ngx-select.module";
+import { NxgSelectComponent } from "./src/ngx-select.component";
+
+export { NgxSelectModule, NxgSelectComponent };
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { NgxSelectModule } from "./ngx-select.module";
-import { NxgSelectComponent } from "./ngx-select.component";
-
-export { NgxSelectModule, NxgSelectComponent };
